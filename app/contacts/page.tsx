f9ad69b04070f8dffe97fb39e00729b5c0cda983
SwatantraYadav <swatantra_y@hotmail.com>
2024-12-28 21:35:52 -0800

    Updated social media links

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaEnvelope, FaSkype } from "react-icons/fa";
+import { FaEnvelope, FaFacebook, FaLinkedin, FaYoutube, FaTwitter, FaInstagramSquare } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -14,18 +14,46 @@ const Contact = () => {
           
           <div className="space-y-6">
             <div className="flex items-center justify-center space-x-4">
-              <FaEnvelope className="text-3xl text-blue-600" />
-              <a href="mailto:metagro.in@gmail.com" className="text-xl text-blue-600 hover:underline">
-                metagro.in@gmail.com
+              <FaEnvelope className="text-3xl text-green-600" />
+              <a href="mailto:sales@metaagro.co.in" className="text-xl text-blue-600 hover:underline">
+                sales@metaagro.co.in
               </a>
             </div>
             
             <div className="flex items-center justify-center space-x-4">
-              <FaSkype className="text-3xl text-blue-600" />
+              <FaLinkedin className="text-3xl text-blue-600" />
+              <a href="https://www.linkedin.com/company/metaagroofficial/" className="text-xl text-blue-600 hover:underline">
+              @metaagroofficial
+              </a>
+            </div>
+
+            <div className="flex items-center justify-center space-x-4">
+              <FaFacebook className="text-3xl text-blue-600" />
+              <a href="https://www.facebook.com/metaagroofficial/" className="text-xl text-blue-600 hover:underline">
+              @metaagroofficial
+              </a>
+              </div>
+
+            <div className="flex items-center justify-center space-x-4">
+              <FaInstagramSquare className="text-3xl text-red-600" />
+              <a href="https://www.instagram.com/metaagroofficial/" className="text-xl text-blue-600 hover:underline">
+              @metaagroofficial
+              </a>
+            </div>
+
+            <div className="flex items-center justify-center space-x-4">
+              <FaYoutube className="text-3xl text-red-600" />
+              <a href="https://www.youtube.com/@metaagroofficial" className="text-xl text-blue-600 hover:underline">
+              @metaagroofficial
+              </a>
+            </div>
+            <div className="flex items-center justify-center space-x-4">
+              <FaTwitter className="text-3xl text-blue-600" />
               <a href="https://bsky.app/profile/metagro.bsky.social" className="text-xl text-blue-600 hover:underline">
               @metagro.bsky.social
               </a>
             </div>
+
           </div>
           
           <div className="mt-12">
